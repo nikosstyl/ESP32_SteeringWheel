6416317e15a8ffba65007d8664b7878e5cc69d95
Nikos Stylianou <stylianou404@gmail.com>
2022-09-10 01:53:34 +0300

    Communication between 2 is OK! 😎

--- ESP32_SteeringWheel_Bluetooth/ESP32_SteeringWheel_Bluetooth.ino
+++ ESP32_SteeringWheel_Bluetooth/ESP32_SteeringWheel_Bluetooth.ino
@@ -1,21 +1,24 @@
-#define IS_DASHBOARD true // Ena define poy orizei thn 8esh ths plaketas alla kai ta pragmata poy prepei na kanei. (Vgainei me undef)
-									// To steeringwheel einai o slave kai to dashboardPCB einai o master.
-#define DEBUG true	// If debug == true, it spits out to serial a bunch of debugging information.
-
 #include "BluetoothSerial.h"
+
+// #undef IS_DASHBOARD
+#define IS_DASHBOARD true // Ena define poy orizei thn 8esh ths plaketas alla kai ta pragmata poy prepei na kanei. (Vgainei me undef)
+						//   To steeringwheel einai o slave kai to dashboardPCB einai o master.
+#define DEBUG true	// If debug == true, it spits out to serial a bunch of debugging information.
 
 BluetoothSerial SerialBT;
 
 String master_name = "DashboardPCB";
 String slave_name = "SteeringWheelPCB";
-String msg;
-int receivedByte=0;
 
 #ifdef IS_DASHBOARD
+String msg;
+char *msgChar=NULL;
+int receivedByte=0, launch=-1, up_shift=-1, down_shift=-1;
+
 void setup() {
 	pinMode(LED_BUILTIN, OUTPUT);
 	digitalWrite(LED_BUILTIN, LOW);
-
+	
 	Serial.begin(115200);
 	SerialBT.begin(master_name, true);
 
@@ -29,15 +32,24 @@ void setup() {
 }
 
 void loop() {
+
 	if (!SerialBT.isClosed() && SerialBT.connected()) {
 		if (SerialBT.available()) {
 			receivedByte = SerialBT.read();
 			msg += (char) receivedByte;
-			if (receivedByte == '\n') {
-				Serial.print(msg);
+			if (receivedByte == '\n') {	// Molis lavei to ending byte, ksekinane oi ypoloipes doyleis (aka CAN Send).
+				msgChar = (char*)calloc(msg.length()+1, sizeof(char));	// Allocate ena meros mnhmhs gia na parw tis times pou stelnei to allo ESP32.
+				msg.toCharArray(msgChar, msg.length());	// Metatroph apo String se char gia na to dexetai h sscanf.
+				sscanf(msgChar, "%d,%d,%d\n", &launch, &up_shift, &down_shift);	// Extracting values sent via Bluetooth
+				free(msgChar);	// Apodesmevoume tis times gia na mhn kanei mpoum to ESP32.
+				
+				/* char output[128];
+				sprintf(output, "Launch:%d\tUPShift:%d\tDownshift:%d", launch, up_shift, down_shift);
+				Serial.println(output); */
 				msg="";
 			}
 		}
+		
 	}
 	else {
 		// The device tries to reconnect if the signal gets lost
@@ -55,6 +67,8 @@ void loop() {
 }
 
 #else // Kodikas gia to SteeringWheelPCB
+char buffer[3*sizeof(int) + 3*sizeof(char)]={'\0'};
+uint8_t launch=0, up_shift=0, down_shift=0;
 
 void setup() {
 	pinMode(LED_BUILTIN, OUTPUT);
@@ -68,14 +82,14 @@ void setup() {
 }
 
 void loop() {	
-	if (Serial.available()) {
-		SerialBT.write(Serial.read());
-	}
-	if (SerialBT.available()) {
-		Serial.write(SerialBT.read());
-	}
+	launch = rand();
+	up_shift = rand();
+	down_shift = rand();
+	sprintf(buffer, "%d,%d,%d\n", launch, up_shift, down_shift);
+	SerialBT.print(buffer);
+	printIfDebug(buffer);
+	delay(10);
 }
-
 #endif
 
 void printIfDebug(String output) {
