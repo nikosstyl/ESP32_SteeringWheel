7bdd6dc0fc1af67f9a84b2cca100a224588311e8
Nikos Stylianou <stylianou404@gmail.com>
2022-09-09 16:41:36 +0300

    c

--- ESP32_SteeringWheel_WiFi/ESP32_SteeringWheel_WiFi.ino
+++ ESP32_SteeringWheel_WiFi/ESP32_SteeringWheel_WiFi.ino
@@ -3,38 +3,60 @@
 #include <WiFiAP.h>
 
 #define DEBUG true
+#define IS_DASHBOARD true
 
 const char *ssid = "DashboardPCB";
 const char *pass = "AlwaysDangerousCRT";
 String receivedMsg="";
 WiFiServer server(80);
 
-void setup() {
-	pinMode(LED_BUILTIN, OUTPUT);
-	digitalWrite(LED_BUILTIN, LOW);
-	Serial.begin(115200);
+#ifdef IS_DASHBOARD
+	void setup() {
+		pinMode(LED_BUILTIN, OUTPUT);
+		digitalWrite(LED_BUILTIN, LOW);
+		Serial.begin(115200);
 
-	WiFI.softAP(ssid, pass);
-	#ifdef DEBUG
-		Serial.println("Created WiFi hotspot");
-	#endif
-}
+		WiFI.softAP(ssid, pass);
+		#ifdef DEBUG
+			Serial.println("Created WiFi hotspot");
+		#endif
+	}
 
-void loop() {
-	WiFiClient client = server.available();
-	#ifdef DEBUG
-		Serial.println("New client connected");
-	#endif
+	void loop() {
+		WiFiClient client = server.available();
+		#ifdef DEBUG
+			Serial.println("New client connected");
+		#endif
 
-	while (client.connected()) {
-		digitalWrite(LED_BUILTIN, HIGH);
-		if (client.available()) {
-			char c = client.read();
-			receivedMsg += c;
-			if (c == '\0') {
-				Serial.println(receivedMsg);
-				receivedMsg = "";
+		while (client.connected()) {
+			digitalWrite(LED_BUILTIN, HIGH);
+			if (client.available()) {
+				char c = client.read();
+				receivedMsg += c;
+				if (c == '\0') {
+					Serial.println(receivedMsg);
+					receivedMsg = "";
+				}
 			}
 		}
 	}
-}
+#else
+	void loop() {
+		pinMode(LED_BUILTIN, OUTPUT);
+		digitalWrite(LED_BUILTIN, LOW);
+		Serial.begin(115200);
+
+		WiFi.begin(ssid,pass);
+		while (WiFi.status() != WL_CONNECTED) {
+			delay(500);
+			#ifdef DEBUG
+
+			#endif
+		}
+	}
+
+	void setup() {
+
+	}
+
+#endif
